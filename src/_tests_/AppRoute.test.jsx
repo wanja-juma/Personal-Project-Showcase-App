c420d5154dfdb74aeb9a3da8e0d1b265a518c521
ruthjuma100@gmail.com <ruthjuma100@gmail.com>
2026-05-18 17:47:23 +0300

    update test and App.jsx files

--- src/_tests_/AppRoute.test.jsx
+++ src/_tests_/AppRoute.test.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+
 import { render, screen } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import AppRoutes from "../Routes/AppRoutes"
--- src/_tests_/Products.test.jsx
+++ src/_tests_/Products.test.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+
 import { render, screen, fireEvent } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import Products from "../Pages/Products"
